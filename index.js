71e980b06ec023d6350c3c6697ea59c6a606835d
Vishnu Dubey <masteroes@gmail.com>
2018-04-25 18:02:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
+  console.log("_____",req.body);
   var speech =
     req.body.result &&
     req.body.result.parameters &&
